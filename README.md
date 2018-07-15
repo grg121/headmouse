a01b689e26300c0013f93c07b4842d737a9e8852
GRG121 <navarromoralesdev@gmail.com>
2018-07-16 01:10:44 +0200

    new start

--- README.md
+++ README.md
@@ -1,30 +1,4 @@
-configuración inicial basada en: https://github.com/dxue2012/python-webcam-flask 
+flask-video-streaming
+=====================
 
-### Setup
-
-#### Optional
-
-- install heroku cli https://devcenter.heroku.com/articles/heroku-cli
-- Use a python virtualenv: (install virtualenv + virtualenv headmouse + source headmouse/bin/activate) 
-
-#### Required
-- `git clone https://github.com/dxue2012/python-webcam-flask.git`
-- `pip install -r requirements.txt`
-
-### Run locally
-
-IF YOU HAVE HEROKU:
-- `heroku local`
-IF NOT:
-- `gunicorn -k eventlet -w 1 app:app --log-file=-`
-
-- in your browser, navigate to localhost:5000
-
-### Deploy to heroku
-
-- `git push heroku master`
-- heroku open
-
-### Common Issues
-
-If you run into a 'protocol not found' error, see if [this stackoverflow answer helps](https://stackoverflow.com/questions/40184788/protocol-not-found-socket-getprotobyname).
+Supporting code for my article [video streaming with Flask](http://blog.miguelgrinberg.com/post/video-streaming-with-flask) and its follow-up [Flask Video Streaming Revisited](http://blog.miguelgrinberg.com/post/flask-video-streaming-revisited).
--- app.py
+++ app.py
@@ -1,30 +1,18 @@
-from sys import stdout
-from makeup_artist import Makeup_artist
-import logging
+#!/usr/bin/env python
+from importlib import import_module
+import os
 from flask import Flask, render_template, Response
-from flask_socketio import SocketIO
-from camera import Camera
-from utils import base64_to_pil_image, pil_image_to_base64
 
+# import camera driver
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
+
+# Raspberry Pi camera module (requires picamera package)
+# from camera_pi import Camera
 
 app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(stdout))
-app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = True
-socketio = SocketIO(app)
-camera = Camera(Makeup_artist())
-
-
-@socketio.on('input image', namespace='/test')
-def test_message(input):
-    input = input.split(",")[1]
-    camera.enqueue_input(input)
-    #camera.enqueue_input(base64_to_pil_image(input))
-
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    app.logger.info("client connected")
 
 
 @app.route('/')
@@ -33,12 +21,10 @@ def index():
     return render_template('index.html')
 
 
-def gen():
+def gen(camera):
     """Video streaming generator function."""
-
-    app.logger.info("starting to generate frames!")
     while True:
-        frame = camera.get_frame() #pil_image_to_base64(camera.get_frame())
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -46,8 +32,9 @@ def gen():
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run(host='0.0.0.0', threaded=True)

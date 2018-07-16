888ad0cedfad21f28af7acd5e3d55870ad324ec4
GRG121 <navarromoralesdev@gmail.com>
2018-07-16 08:54:50 +0200

    favicon

--- camera_opencv.py
+++ camera_opencv.py
@@ -1,6 +1,14 @@
 import cv2
 from base_camera import BaseCamera
 
+import numpy as np
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
 
 class Camera(BaseCamera):
     video_source = 0
@@ -18,6 +26,21 @@ class Camera(BaseCamera):
         while True:
             # read current frame
             _, img = camera.read()
+
+            img =  cv2.flip( img, 1 )
+
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+            for (x,y,w,h) in faces:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = img[y:y+h, x:x+w]
+
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
             # encode as a jpeg image and return it
             yield cv2.imencode('.jpg', img)[1].tobytes()
